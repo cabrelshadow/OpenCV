7f0838a853cbde6746d44d884a870e241b5b32f3
Yvan_Nguewo <yvannjopmo85@gmail.com>
2022-11-12 12:21:29 +0100

    creation des tables experiences et experiences professionalle

---
+++ objects/tables/table_experience.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Experience
+(
+    id_exp INT NOT NULL,
+    title_exp VARCHAR(50) NOT NULL,
+    id_user INT NOT NULL,
+    id_cv INT NOT NULL,
+);
+        --- BY Yvan Njopmo ---
+    
---
+++ objects/tables/table_experiencePro.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Professional_Exp
+(
+    id_expro INT NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    organisation VARCHAR(50) NOT NULL,
+    function VARCHAR(50) NOT NULL,
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL,
+    id_exp INT NOT NULL
+);
+
+        --- BY Yvan Njopmo ---
