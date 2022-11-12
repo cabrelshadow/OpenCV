f8b46e58db6e0d5895e25dc8d272aad38e0d1e3a
pokemfeze <maximefeze1@gmail.com>
2022-11-12 12:22:46 +0100

    creation des tables profil city et country

--- objects/tables/contraints/runContraints.sql
+++ objects/tables/contraints/runContraints.sql
@@ -25,3 +25,12 @@ PROMPT******************** add constrains_pk_Projet*****************************
 
 PROMPT******************** add constrains_pk_SocialNetwork*************************************
 @objects/tables/contraints/pk/pk_SocialNetwork
+
+PROMPT******************** add constrains_pk_Profil*************************************
+@objects/tables/contraints/pk/pk_Profil
+
+PROMPT******************** add constrains_pk_City*************************************
+@objects/tables/contraints/pk/pk_City
+
+PROMPT******************** add constrains_pk_Country*************************************
+@objects/tables/contraints/pk/pk_Country
---
+++ objects/tables/table_Country.sql
@@ -0,0 +1,7 @@
+-- create table country
+CREATE TABLE Country
+(
+    id_country INT NOT NULL,
+    name_country VARCHAR(30) NOT NULL
+);
+-- BY POKEM FEZE MAXIME
