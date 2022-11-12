db71b5af3e7ab907c5d9527f2470b0570867ae31
unknown <wilfrieddzomeu90@gmail.com>
2022-11-12 13:36:33 +0100

    ajout de la table phone

---
+++ objects/tables/table_Phones.sql
@@ -0,0 +1,10 @@
+create table Phones 
+(
+    id_socialnet int ,
+    phoneNumber int ,
+    IsMobile varchar (50),
+    IsWhatsapp varchar(50),
+    IsTelegram varchar(50),
+    countryCode varchar(50),
+    id_profil int 
+);
