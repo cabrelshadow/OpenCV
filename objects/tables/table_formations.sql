904ab20929538eee8346f50e230c9996fbd3bd05
Yvan_Nguewo <yvannjopmo85@gmail.com>
2022-11-12 12:19:35 +0100

    creation de la table formation

---
+++ objects/tables/table_formations.sql
@@ -0,0 +1,16 @@
+
+ CREATE TABLE Formation
+ (
+    id_formation INT NOT NULL,
+    school varchar (50) NOT NULL, 
+    fieldOfStudy varchar (50) NOT NULL, 
+    diploma varchar(50) NOT NULL,
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL,
+    obtenedResult varchar(50) NOT NULL,
+    descriptionForm varchar(100) NOT NULL,
+    id_user INT NOT NULL,
+    id_exp INT NOT NULL,
+    id_cv INT NOT NULL,
+);
+        --- BY Yvan Njopmo ---
