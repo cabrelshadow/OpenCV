f1f9678ec1acd1033d21baf3a79ea4ef5bfa7d82
nathan <njahanathan2>
2022-11-12 12:03:51 +0100

    modification des des contraintes

--- objects/tables/contraints/runContraints.sql
+++ objects/tables/contraints/runContraints.sql
@@ -5,8 +5,23 @@ PROMPT##########################################################################
 
 PROMPT******************** add constrains_pk_user*************************************
 @objects/tables/contraints/pk/pk_user
+
+
 PROMPT******************** add constrains_pk_language*************************************
 @objects/tables/contraints/pk/pk_language
 
+
 PROMPT******************** add constrains_pk_cv*************************************
 @objects/tables/contraints/pk/pk_cv
+
+
+PROMPT******************** add constrains_pk_Hobbies*************************************
+@objects/tables/contraints/pk/pk_Hobbies
+
+
+PROMPT******************** add constrains_pk_Projet*************************************
+@objects/tables/contraints/pk/pk_Projet
+
+
+PROMPT******************** add constrains_pk_SocialNetwork*************************************
+@objects/tables/contraints/pk/pk_SocialNetwork
