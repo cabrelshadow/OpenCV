e1fa841c857fc2b0276361e3c8f265ba6c711ba9
pokemfeze <maximefeze1@gmail.com>
2022-11-13 22:13:53 +0100

    ajout des data dans la table country et le run de data

---
+++ data/data_country.sql
@@ -0,0 +1,12 @@
+--insertion des donnee de la table country
+INSERT INTO Country (id_country,name_country,name_city,id_profil) values (01,'cameroun','yaounde',0001);
+INSERT INTO Country (id_country,name_country,name_city,id_profil) values (02,'france','paris',0002);
+INSERT INTO Country (id_country,name_country,name_city,id_profil) values (03,'canada','toronto',0003);
+INSERT INTO Country (id_country,name_country,name_city,id_profil) values (04,'cameroun','douala',0004);
+INSERT INTO Country (id_country,name_country,name_city,id_profil) values (05,'benin','cotonou',0005);
+INSERT INTO Country (id_country,name_country,name_city,id_profil) values (06,'espagne','valencia',0006);
+INSERT INTO Country (id_country,name_country,name_city,id_profil) values (07,'cameroun','bertoua',0007);
+INSERT INTO Country (id_country,name_country,name_city,id_profil) values (08,'cameroun','kribi',0008);
+INSERT INTO Country (id_country,name_country,name_city,id_profil) values (09,'canada','moncton',0009);
+INSERT INTO Country (id_country,name_country,name_city,id_profil) values (10,'cameroun','garoua',0010);
+--BY POKEM FEZE MAXIME 
