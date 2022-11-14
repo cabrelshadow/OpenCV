971d95fef7ec03c8a747eb55b14f42b73cdbf1b7
sianou <sianou93@gmail.com>
2022-11-14 19:00:14 +0100

    add sequence file and feature darectory

---
+++ sequenceTable.sql
@@ -0,0 +1,12 @@
+---sequece table user---
+create sequence sec_user
+start with 100
+increment by 1;
+-------sequece table cv---
+create sequence sec_cv
+start with 100
+increment by 1;
+------ sequence table language---
+create sequence sec_user
+start with 100
+increment by 1;
