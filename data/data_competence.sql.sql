810771befa48d0a0749efe73453d12590361f425
JordanNONO <jordannono2245@gmail.com>
2022-11-14 23:04:21 +0100

    enregistrement des donnees dans les tables competence et recommandation

---
+++ data/data_competence.sql.sql
@@ -0,0 +1,15 @@
+--
+
+--insertion des donnees dans la table Competence /insert data in the table Competence by jordan Nono
+
+--
+INSERT INTO Competence (id_Competence,name_competence,level_competence,id_recommandation,id_hobbies,id_formation,id_cv) values (01,'HTML5','beginner',001,1,1,1);
+INSERT INTO Competence (id_Competence,name_competence,level_competence,id_recommandation,id_hobbies,id_formation,id_cv) values (02,'CSS3','medium',002,2,2,2);
+INSERT INTO Competence (id_Competence,name_competence,level_competence,id_recommandation,id_hobbies,id_formation,id_cv) values (03,'JAVA','beginner',003,3,3,3);
+INSERT INTO Competence (id_Competence,name_competence,level_competence,id_recommandation,id_hobbies,id_formation,id_cv) values (04,'JAVASCRIPT','beginner',004,4,4,4);
+INSERT INTO Competence (id_Competence,name_competence,level_competence,id_recommandation,id_hobbies,id_formation,id_cv) values (05,'ADO/ASP dot net','medium',005,5,5,5);
+INSERT INTO Competence (id_Competence,name_competence,level_competence,id_recommandation,id_hobbies,id_formation,id_cv) values (06,'UML','expert',006,6,6,6);
+INSERT INTO Competence (id_Competence,name_competence,level_competence,id_recommandation,id_hobbies,id_formation,id_cv) values (07,'C#','expert',007,7,7,7);
+INSERT INTO Competence (id_Competence,name_competence,level_competence,id_recommandation,id_hobbies,id_formation,id_cv) values (08,'ANDROID','expert',008,8,8,8);
+INSERT INTO Competence (id_Competence,name_competence,level_competence,id_recommandation,id_hobbies,id_formation,id_cv) values (09,'SQL','expert',009,9,9,9);
+INSERT INTO Competence (id_Competence,name_competence,level_competence,id_recommandation,id_hobbies,id_formation,id_cv) values (10,'C++','expert',010,10,10,10);
