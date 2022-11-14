09629f930fd812aebf1f5d6b2197c2eeff726825
unknown <wilfrieddzomeu90@gmail.com>
2022-11-14 17:40:30 +0100

    ajout de data_phones

---
+++ data/data_phones.sql
@@ -0,0 +1,8 @@
+--created by nguenou --
+INSERT INTO Phones(id_socialnet,phoneNumber,IsMobile,IsWhatsapp,IsTelegram,countryCode,id_profil)VALUES( 01, 679340245, 'mtn','orange','tele',237,001) ;
+
+
+INSERT INTO Phones(id_socialnet,phoneNumber,IsMobile,IsWatsapp,IsTelegram,countryCode,id_profil)VALUES(02,678909876,'maxime','pok_max','yaounde',345,002);
+
+
+INSERT INTO Phones(id_socialnet , phoneNumber,IsMobile,IsWhatsapp,IsTelegram,countryCode,id_profil)VALUES( 03, 679340245, 'mtn','orange','tele',237,003) ;
