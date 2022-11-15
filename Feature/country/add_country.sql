d193ed602ac7f1504bc7d508745fd932d7a2c343
pokemfeze <maximefeze1@gmail.com>
2022-11-15 21:33:01 +0100

    modification de la fonctionnalite add de la table country

--- Feature/country/add_country.sql
+++ Feature/country/add_country.sql
@@ -1,2 +1,2 @@
 INSERT INTO Country(id_country,name_country,name_city,id_profil) 
-values(&id_country,'&name_country','name_city',&id_profil);
+values(&id_country,'&name_country','&name_city',&id_profil);
