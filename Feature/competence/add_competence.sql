e8c513a0069a80cb307aabf0785d031a7b274f67
JordanNONO <jordannono2245@gmail.com>
2022-11-16 19:45:46 +0100

    update feature

--- Feature/competence/add_competence.sql
+++ Feature/competence/add_competence.sql
@@ -4,6 +4,6 @@
 
 --
 INSERT INTO Competence(id_Competence,name_competence,level_competence,id_recommandation,id_hobbies,id_formation,id_cv)
-VALUES('&id_Competence','&name_competence','&level_competence','&id_recommandation','&id_hobbies','id_formation','id_cv');
+VALUES(&id_Competence,'&name_competence','&level_competence',&id_recommandation,&id_hobbies,id_formation,id_cv);
 
 SELECT * FROM Competence;
