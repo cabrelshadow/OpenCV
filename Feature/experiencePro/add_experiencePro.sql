2ef566b9f7e6f597d221c64924454828d507fc27
Yvan_Nguewo <yvannjopmo85@gmail.com>
2022-11-17 17:44:30 +0100

    fonctionnalite d'ajout dans les tables experiences,experience pro et formation

--- Feature/experiencePro/add_experiencePro.sql
+++ Feature/experiencePro/add_experiencePro.sql
@@ -2,6 +2,9 @@
 INSERT INTO Professional_Exp(id_expro, status,organisation,function, startDate, endDate, id_exp)
 VALUES (&id_expro,'&status','&organisation','&function','&startDate','&endDate',&id_exp);
 
-SELECT* FROM Professional_Exp;
+SELECT* 
+FROM Professional_Exp 
+WHERE id_expro =&id_expro
+AND id_exp=&id_exp;
 
              ----By Yvan Njopmo---
--- Feature/formation/add_formation.sql
+++ Feature/formation/add_formation.sql
@@ -2,6 +2,11 @@
 INSERT INTO  Formation (id_formation, school,fieldOfStudy,diploma,startDate,endDate,obtenedResult,descriptionForm,id_user,id_exp,id_cv)
 VALUES(&id_formation, '&school','&fieldOfStudy','&diploma','&startDate','&endDate','&obtenedResult','&descriptionForm',&id_user,&id_exp,&id_cv);
 
-SELECT* FROM Formation;
+SELECT* 
+FROM Formation 
+WHERE id_formation=&id_formation
+AND id_user =&id_user 
+AND id_exp =&id_exp
+AND id_cv =&id_cv;
 
         ----By Yvan Njopmo--
