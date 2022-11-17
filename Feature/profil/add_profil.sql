38dc6b7ac05d283c30c091fc8f845dd877c58b51
pokemfeze <maximefeze1@gmail.com>
2022-11-17 17:14:40 +0100

    modification de la fonctionnalite add  de la table profil

--- Feature/profil/add_profil.sql
+++ Feature/profil/add_profil.sql
@@ -1,4 +1,4 @@
 INSERT INTO Profil(id_profil,first_name,last_name,profile_name,profile_image,born_date,born_At,marital_status,numberChild,driving_licence,id_user)
 VALUES (&id_profil,'&first_name','&last_name','&profile_name','&profile_image','&born_date','&born_at','&marital_status',&numberChild,'&driving_licence',&id_user);
 
-SELECT * FROM Profil;
+SELECT * FROM Profil WHERE id_user=&id_user;
