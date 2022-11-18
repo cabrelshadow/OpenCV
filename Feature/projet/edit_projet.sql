651a7f20d709c1000ac85bfae824242fa8b26079
nathan <njahanathan2>
2022-11-18 13:13:59 +0100

    rectification des fonctionnalite

--- Feature/projet/edit_projet.sql
+++ Feature/projet/edit_projet.sql
@@ -1,14 +1,14 @@
 -- by Njaha --
 UPDATE projet
-SET id_projet=&id_projet AND
-projet_name='&projet_name' AND
-projet_status='&projet_status' AND
-startdate='&startdate' AND
-enddate='&enddate' AND
-partners='&partners' AND
-urlprojet='&urlprojet' AND
-description_projet='&description_projet' AND
-id_formation=&id_formation AND
-id_exp=&id_exp AND
-id_expro=&id_expro 
+SET 
+projet_name='&projet_name',
+projet_status='&projet_status',
+startdate='&startdate',
+enddate='&enddate',
+partners='&partners',
+urlprojet='&urlprojet',
+description_projet='&description_projet',
+id_formation=&id_formation,
+id_exp=&id_exp,
+id_expro=&id_expro,
 WHERE id_projet=&id_projet;
---
+++ Feature/socialnetwork/edit_socialnetwork.sql
@@ -0,0 +1,8 @@
+UPDATE socialnetwork
+SET 
+title_internet='title_internet',
+image_internet='&image_internet',
+id_profil=&id_profil
+
+WHERE id_internet = &id_internet;
+     ---- By NJAHA ---
