fff0bf93b6632933e774fdc7fad55a9d26fbed92
sianou <sianou93@gmail.com>
2022-11-18 14:14:39 +0100

    fix profile feature

--- Feature/showCV.sql
+++ Feature/showCV.sql
@@ -4,8 +4,8 @@ select * from users WHERE username = '&&user_name' AND  password = '&&user_passw
 
 SELECT first_name,last_name,profile_name,profile_image,born_date,born_At,marital_status,numberChild,driving_licence
 FROM Profil 
-WHERE id_user = (select user_id FROM users WHERE user_name = '&user_name' AND user_password = '&user_password');
+WHERE id_user = (select id_user FROM users WHERE username = '&user_name' AND password = '&user_password');
 
 SELECT name_country,name_city
 FROM Country 
-WHERE id_user = (select user_id FROM users WHERE user_name = '&user_name' AND user_password = '&user_password');
+WHERE id_profil=&id_profil;
