3b2cbbbb318252a4fe6a18929a4936919efabd28
nathan <njahanathan2>
2022-11-18 17:56:45 +0100

    ajout de la fonctinnalite afficher le cv

--- Feature/showCV.sql
+++ Feature/showCV.sql
@@ -82,7 +82,114 @@ WHERE F.id_user = (select id_user FROM users WHERE username = '&user_name' AND p
 
 
 
+SET MARKUP HTML ON
+SPOOL ON 
+SET NULL'_'
+SET TAB OFF
+SET LINESIZE 1000
+SET NEWPAGE NONE
+SET TRIMS OFF
+SET NUHWIDTN 50
+SILENT
+SPOOL CV_EXEMPLE.html
 
+
+PROMPT ********************************************************************PROFILE INFORMATIONS********************************************************************
+
+SELECT first_name "Nom",last_name "Prenom",profile_name "Nom du profile",profile_image "Images",born_date "Date de naissance",born_At "Lieu de Naissance",marital_status "Statut Matrimonial",numberChild "Nombre D'enfant",driving_licence " Categorie de Permis de Ceonduit"
+FROM Profil 
+WHERE id_user = (select id_user FROM users WHERE username = '&user_name' AND password = '&user_password');
+
+PROMPT ********************************************************************Country INFORMATIONS********************************************************************
+
+SELECT c.name_country "Nom de Pays", c.name_city "Nom de La Ville"
+FROM users u  JOIN  Profil p
+ON (u.id_user=p.id_user)
+JOIN Country c
+ON(p.id_profil=c.id_profil)
+WHERE p.id_user =(select id_user FROM users  WHERE username = '&user_name' AND  password = '&user_password');
+
+
+PROMPT ********************************************************************Phones INFORMATIONS********************************************************************
+
+SELECT pe.phoneNumber "Numero de Telephone", pe.IsMobile "Numero Fixe", pe.IsWhatsapp "Numero Whatsapp", pe.IsTelegram "Numero Telegrame", pe.countrycode "Indicatif telephonique"
+FROM users u  JOIN  Profil p
+ON (u.id_user=p.id_user)
+JOIN Phones pe
+ON(p.id_profil=pe.id_profil)
+WHERE p.id_user =(select id_user FROM users  WHERE username = '&user_name' AND  password = '&user_password');
+
+PROMPT ********************************************************************SocialNetwork INFORMATIONS********************************************************************
+
+SELECT sn.title_internet "Titre du reseau social", sn.image_internet "Icone"
+FROM users u  JOIN  Profil p
+ON (u.id_user=p.id_user)
+JOIN socialnetwork sn
+ON(p.id_profil=sn.id_profil)
+WHERE p.id_user =(select id_user FROM users  WHERE username = '&user_name' AND  password = '&user_password');
+
+PROMPT ********************************************************************Formation INFORMATIONS********************************************************************
+
+SELECT school "Ecoles",fieldOfStudy "Domaine d'etude",diploma "Diplome",startDate "Date de debut",endDate "Date de fin",obtenedResult "Resultat Obtenus",descriptionForm "Description de la formation"
+FROM Formation
+WHERE id_user = (select id_user FROM users WHERE username = '&user_name' AND password = '&user_password');
+
+PROMPT ********************************************************************LANGUAGES INFORMATIONS********************************************************************
+
+SELECT l.title "Nom de Langue", l.levels "Niveau D'etudes"
+FROM users u  JOIN  Profil p
+ON (u.id_user=p.id_user)
+JOIN Language l
+ON(p.id_profil=l.id_profil)
+WHERE p.id_user =(select id_user FROM users  WHERE username = '&user_name' AND  password = '&user_password');
+
+PROMPT ********************************************************************EXPERIENCES INFORMATIONS********************************************************************
+
+SELECT title_exp "Titre de L'Experience"
+FROM Experience
+WHERE id_user = (select id_user FROM users WHERE username = '&user_name' AND password = '&user_password');
+
+SELECT pr.status "Status Occupee", pr.organisation "Organisation de travail", pr.function "Fonctionnalite",pr.startDate "Date de debut", pr.endDate "Date de fin"
+FROM users u  JOIN  Experience exp
+ON (u.id_user=exp.id_user)
+JOIN professional_exp pr 
+ON(pr.id_exp=exp.id_exp)
+WHERE exp.id_user =(select id_user FROM users  WHERE username = '&user_name' AND  password = '&user_password');
+
+
+PROMPT ********************************************************************Competence INFORMATIONS********************************************************************
+
+SELECT cp.name_competence "Competences", cp.level_competence "Niveau de competence"
+FROM users u  
+JOIN Formation F 
+on(u.id_user=F.id_user)
+JOIN Competence cp 
+ON(F.id_formation= cp.id_formation)
+WHERE F.id_user = (select id_user FROM users WHERE username = '&user_name' AND password = '&user_password');
+
+PROMPT ********************************************************************PROJET INFORMATIONS********************************************************************
+
+SELECT pj.projet_name "Nom Du Projet", pj.projet_status "Niveau de projet", pj.startDate "Date de debut", pj.endDate "Date de fin", pj.partners "Partenaire", pj.urlprojet "URL", pj.description_projet "Description du projet"
+FROM users u  JOIN  Experience exp
+ON (u.id_user=exp.id_user)
+JOIN projet pj 
+ON(pj.id_exp=exp.id_exp)
+WHERE exp.id_user =(select id_user FROM users  WHERE username = '&user_name' AND  password = '&user_password');
+
+PROMPT ********************************************************************Hobbies INFORMATIONS********************************************************************
+
+SELECT type_hobbies "Types d'hobbies", title_hobbies "Titre du hobbie"
+FROM hobbies
+WHERE id_user = (select id_user FROM users WHERE username = '&user_name' AND password = '&user_password');
+
+PROMPT ********************************************************************Recommandation INFORMATIONS********************************************************************
+
+SELECT personneName "Nom du Mentor", relationShipLevel "Niveau Relationnel",postAtTheTime "Poste actuel"
+FROM Recommandation
+WHERE id_user = (select id_user FROM users WHERE username = '&user_name' AND password = '&user_password');
+
+SET MARKUP HTML OFF
+SPOOL OFF
 
 
 
